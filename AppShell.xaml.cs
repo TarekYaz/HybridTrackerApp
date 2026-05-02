b981b8f08a1a737331ba01fa1196256d4369ba48
TarekYaz <markyazbeck94@gmail.com>
2026-05-02 13:18:51 +0100

    added maui starter project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HybridTrackerApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HybridTrackerApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
